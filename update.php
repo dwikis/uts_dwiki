4935494f1dce1c7daec01d475b9ee6a7eaa2a1c6
Your Name <you@example.com>
2018-04-03 16:34:34 +0700

    Dwiki S

---
+++ update.php
@@ -0,0 +1,34 @@
+<?php
+include "koneksi.php";
+
+$koneksiObj = new koneksi();
+$koneksi = $koneksiObj->getKoneksi();
+
+if($koneksi->connect_error) {
+    echo "gagal koneksi : ".$koneksi->connect_error;
+}else {
+    echo "sambungan basis data berhasil";
+}
+
+//update stok_barang
+//set nama_barang = '{namaBarang}',
+// stok = {stok}
+// where kode = {kode};
+
+$nim 	= $_POST['nim'];
+$nama   = $_POST['nama'];
+$jurusan = $_POST['jurusan'];
+
+$query = "update mahasiswa set nama = '$nama', jurusan = '$jurusan' where nim = '$nim'";
+        echo $query;
+
+if($koneksi->query($query) === true) {
+    echo "<br> Data ". $_POST["nama"]. " berhasil Diubah". 
+	'<a href="main.php">Lihat Data</a>';
+}else{
+    $koneksi->error;
+    echo "<br>Data gagal disimpan" ;
+}
+
+$koneksi->close();
+?>
